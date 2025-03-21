faa84e9143a2e0ee8a661886f132b2d0c0644389
Akshay_G <manjuakshu47@gmail.com>
2025-03-21 21:27:48 +0530

    Added Second.jsx and updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Second } from './Second';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+  <Second/>
+  <h1> for Q naati</h1>
+   </>
   );
 }
 
---
+++ frontend/src/Second.jsx
@@ -0,0 +1,7 @@
+export const Second=()=>{
+    return(
+        <>
+        <h1>hello gubald</h1>
+        </>
+    )
+}
